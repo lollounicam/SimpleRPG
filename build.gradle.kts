bc4b38b25bfc37d2ad0b54a8216742f9881b32bf
Lorenzo Pompili <lorenzo.pompili@studenti.unicam.it>
2026-05-07 21:46:01 +0200

    Initial creating project commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    application
+}
+
+group = "it.unicam.cs.mpgc.rpg119163"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "it.unicam.cs.mpgc.rpg119163.Main"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
