7698802dd6b7ca8143fd4ce603e575375369ace8
Lorenzo Pompili <lorenzo.pompili@studenti.unicam.it>
2026-05-12 13:58:14 +0200

    Added random enemy creation to game factory

--- src/main/java/it/unicam/cs/mpgc/rpg119163/model/GameFactory.java
+++ src/main/java/it/unicam/cs/mpgc/rpg119163/model/GameFactory.java
@@ -5,15 +5,17 @@ import it.unicam.cs.mpgc.rpg119163.model.characters.Player;
 import it.unicam.cs.mpgc.rpg119163.model.items.HealingPotion;
 import it.unicam.cs.mpgc.rpg119163.model.items.Weapon;
 
+import java.util.List;
+
 public class GameFactory {
-    public Game createNewGame(String playerName) {
+    public Game createNewGame(final String playerName) {
         final Player player = this.createPlayer(playerName);
         final Enemy enemy = this.createEnemy();
 
         return new Game(player, enemy);
     }
 
-    private Player createPlayer(String playerName){
+    private Player createPlayer(final String playerName) {
         final Player player = new Player(playerName, 100, 20, 5);
 
         this.createStarterItems(player);
@@ -22,9 +24,17 @@ public class GameFactory {
     }
 
     private Enemy createEnemy() {
-        return new Enemy("Goblin", 50, 10, 3, 25, 8);
+        final List<Enemy> enemies = List.of(
+                new Enemy("Goblin", 50, 10, 3, 25, 8),
+                new Enemy("Orc", 80, 15, 5, 40, 12),
+                new Enemy("Skeleton", 60, 12, 4, 30, 10)
+        );
+
+        return enemies.get((int) (Math.random() * enemies.size()));
     }
-    private void createStarterItems(Player player) {
+
+
+    private void createStarterItems(final Player player) {
         player.addItem(
                 new HealingPotion("Potion", "Heals 50 HP", 10, 50)
         );
@@ -32,4 +42,6 @@ public class GameFactory {
                 new Weapon("Iron Sword", "A basic iron sword", 50, 10)
         );
     }
+
+
 }
